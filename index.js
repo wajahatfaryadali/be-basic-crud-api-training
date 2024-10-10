31c7fdeddc853ca0842fd0e6cc89cd19ce240dd0
wajahat ali <wajahatfaryadali@gmail.com>
2024-10-10 14:48:42 +0500

    endpoint added to get single product by id

--- index.js
+++ index.js
@@ -13,7 +13,32 @@ app.get('/', (req, res) => {
 })
 
 // api to get all created prodcuts
+app.get('/api/product', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({
+            data: products,
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
+// api to get prodcut by id
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json({
+            data: product,
+        })
+    } catch (error) {
+        console.log('error : ', error)
+        res.status(404).json({ message: "Product not found!" })
+    }
+})
+
+// api to add new product
 app.get('/api/product', async (req, res) => {
     try {
         const products = await Product.find({})
