3a124f89cd4babeaa26b7b8b7e2f17a4a44056b0
wajahat ali <wajahatfaryadali@gmail.com>
2024-10-10 13:52:13 +0500

    model added for product

---
+++ models/product.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Product Name is required!']
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        image: {
+            type: String,
+            required: false,
+        },
+
+        isAvailable: {
+            type: Boolean,
+            required: true,
+            default: true,
+        }
+    },
+    {
+        Timestamp: true
+    }
+)
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
